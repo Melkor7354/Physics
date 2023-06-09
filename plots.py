67b524180c92887165554ada4ce0893fea3db20d
Nazgul7354 <120272677+Melkor7354@users.noreply.github.com>
2023-06-09 16:19:55 +0530

    No.1

--- plots.py
+++ plots.py
@@ -1,3 +1,4 @@
+import numpy
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
@@ -18,7 +19,7 @@ def plot1():
     plt.xlabel("Time")
     plt.grid(axis='both', color='grey')
     ax.set_facecolor("black")
-    ax.set_xlim(-1, 1)
+    ax.set_xlim(-10, 10)
     ax.set_ylim(-10, 10)
     fig.set_facecolor("#45ADA8")
     # Create axes for frequency and amplitude sliders
@@ -46,14 +47,78 @@ def plot1():
 
 def electrostatics1():
     fig, ax = plt.subplots()
-    plt.subplots_adjust(bottom=0.35)
-    x = np.arange(0.0, 1.0, 0.01)
-    y = (9 * 10**9 * 1 * 1)/(x**2)
-    p, = plt.plot(x, y)
+    x = np.arange(0.0001, 1000.0, 0.01)
+    y = (1)/((x)**2)
+    ax.plot(x, y, color='red', lw=3)
+    plt.title(r'$\frac{kq_1q_2}{r^2}$', fontsize=20)
+    plt.axhline(linewidth=2, color='grey')
+    plt.axvline(linewidth=2, color='grey')
+    plt.ylabel("Displacement", fontsize=12)
+    plt.xlabel("Time", fontsize=12)
+    plt.grid(axis='both', color='grey')
+    ax.set_facecolor("black")
+    ax.set_xlim(-2, 10)
+    ax.set_ylim(-2, 10)
+    fig.set_facecolor("#45ADA8")
+
     plt.show()
 
 def electrostatics2():
-    fig, ax = plt.subplots()
-    plt.subplots_adjust(bottom=0.35)
-    x = np.arange()
+    k = 9 * (10 ** 4)
+    def f(g, R):
+        if g < R:
+            return 0.3
+        elif g == R:
+            return k/R**2
+        else:
+            return k/g**2
 
+    fig, ax = plt.subplots()
+    x = np.arange(0, 200, 0.01)
+    y = []
+    for i in range(x.size):
+        y.append(f(x[i], 50))
+    ax.plot(x, y, color='red', lw=3)
+    plt.title("Electric Field for a hollow sphere", fontsize=20)
+    plt.axhline(linewidth=2, color='grey')
+    plt.axvline(linewidth=2, color='grey')
+    plt.ylabel("Electric Field", fontsize=12)
+    plt.xlabel("Radius (r)", fontsize=12)
+    plt.grid(axis='both', color='grey')
+    ax.set_facecolor("black")
+    ax.set_xlim(-5, 200)
+    ax.set_ylim(-5, 60)
+    fig.set_facecolor("#45ADA8")
+    plt.annotate('''r=R
+    Maximum E''', (50, k/50**2), color='red', textcoords='offset points', xytext=(0, 5), ha='center')
+    plt.show()
+
+def electrostatics3():
+    k = 9 * (10 ** 2)
+    def f(g, R):
+        if abs(g) < R:
+            return k/R
+        elif abs(g) > R:
+            return k/abs(g)
+
+    fig, ax = plt.subplots()
+    x = np.arange(-200, 200, 0.01)
+    y = []
+    for i in range(x.size):
+        y.append(f(x[i], 50))
+    ax.plot(x, y, color='red', lw=3)
+    plt.title("Electric Field for a hollow sphere", fontsize=20)
+    plt.axhline(linewidth=2, color='grey')
+    plt.axvline(linewidth=2, color='grey')
+    plt.ylabel("Electric Field", fontsize=12)
+    plt.xlabel("Radius (r)", fontsize=12)
+    plt.grid(axis='both', color='grey')
+    ax.set_facecolor("black")
+    ax.set_xlim(-5, 200)
+    ax.set_ylim(-5, 60)
+    fig.set_facecolor("#45ADA8")
+    plt.annotate('''r=R
+    Maximum E''', (50, k/50**2), color='red', textcoords='offset points', xytext=(0, 5), ha='center')
+    plt.show()
+
+electrostatics3()
